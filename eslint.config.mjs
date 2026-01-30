081c98176f8429d13a612bce6fd52a002474a0c1
wiinuk <wii.nukegara@gmail.com>
2026-01-30 14:04:55 +0900

    ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import {
+    defineConfig,
+    globalIgnores,
+} from "eslint/config";
+
+import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import js from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import {
+    FlatCompat,
+} from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+        },
+
+        ecmaVersion: 2021,
+        sourceType: "module",
+        parserOptions: {},
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    rules: {},
+}, globalIgnores(["**/node_modules/", "**/*.user.js", "**/build.mjs"])]);
