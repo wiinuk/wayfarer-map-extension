f5ca114eb13e9fdfc4afd3eb74702a627a0f842b
wiinuk <wii.nukegara@gmail.com>
2026-01-30 12:43:07 +0900

    tampermonkey 用のデバッグスクリプトテンプレートを追加

---
+++ tampermonkey-debug-wrapper.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Tampermonkey debug wrapper
+// @description  A debug wrapper for referencing local scripts
+// @version      1.0
+// @namespace    http://tampermonkey.net/
+// @match        https://wayfarer.nianticlabs.com/new/mapview
+// @require      file:////wsl.localhost/<path to your local repository>/main.user.js
+// @grant        none
+// ==/UserScript==
