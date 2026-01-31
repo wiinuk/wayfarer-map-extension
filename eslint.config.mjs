ff4e9c23be619a094cf21d777f3bdc0cde4e44e0
wiinuk <wii.nukegara@gmail.com>
2026-01-31 10:15:53 +0900

    ESLint設定のインポート文を整理し、コードの可読性を向上させる

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,4 @@
-import {
-    defineConfig,
-    globalIgnores,
-} from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 import tsParser from "@typescript-eslint/parser";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
@@ -10,9 +7,7 @@ import js from "@eslint/js";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import {
-    FlatCompat,
-} from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,27 +15,33 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    languageOptions: {
-        parser: tsParser,
+export default defineConfig([
+    {
+        languageOptions: {
+            parser: tsParser,
 
-        globals: {
-            ...globals.node,
-            ...globals.browser,
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+
+            ecmaVersion: 2021,
+            sourceType: "module",
+            parserOptions: {},
         },
 
-        ecmaVersion: 2021,
-        sourceType: "module",
-        parserOptions: {},
-    },
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+        extends: compat.extends(
+            "eslint:recommended",
+            "plugin:@typescript-eslint/recommended",
+        ),
+        rules: {},
     },
-
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
-    rules: {},
-}, globalIgnores(["**/node_modules/", "**/*.user.js", "**/build.mjs"])]);
+    globalIgnores(["**/node_modules/", "**/*.user.js", "**/build.mjs"]),
+]);
