817338c59a8e961604a2ce36e5b3e72289b86ffe
wiinuk <wii.nukegara@gmail.com>
2026-02-06 22:42:07 +0900

    Worker上からgcs記録処理を呼び出すように設定

---
+++ source/poi-records.worker.ts
@@ -0,0 +1,47 @@
+// spell-checker: ignore comlink
+import * as Comlink from "comlink";
+import { createGcsHandler } from "./gcs-recorder";
+import {
+    createTypedEventTarget,
+    type TypedCustomEvent,
+} from "./typed-event-target";
+import type { PageEventMap } from "./page-events";
+
+export interface WorkerApi {
+    hello(message: string): string;
+    onGcsReceived(url: string, responseText: string): void;
+}
+
+function handleAsyncError(reason: unknown) {
+    console.error("An error occurred during asynchronous processing:", reason);
+}
+async function setup() {
+    const mainAPI = Comlink.wrap<import("./setup").MainApi>(
+        self as Comlink.Endpoint,
+    );
+
+    const events = createTypedEventTarget<PageEventMap>();
+    const transferEvent = (
+        e: TypedCustomEvent<
+            keyof PageEventMap,
+            PageEventMap[keyof PageEventMap]
+        >,
+    ) => mainAPI.dispatchEvent(e.type, e.detail);
+
+    (["gcs-received", "gcs-saved"] as const).forEach((type) =>
+        events.addEventListener(type, transferEvent),
+    );
+
+    const handler = await createGcsHandler(events, handleAsyncError);
+    const api: WorkerApi = {
+        hello(e) {
+            return `Worker received: ${e}`;
+        },
+        onGcsReceived(url, responseText) {
+            handler(new URL(url), responseText);
+        },
+    };
+    Comlink.expose(api);
+}
+
+setup().catch(handleAsyncError);
