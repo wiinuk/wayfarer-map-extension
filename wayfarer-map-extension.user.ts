d65fc8f49371e8f80198c3eea199af8a1b83e787
wiinuk <wii.nukegara@gmail.com>
2026-06-13 06:27:18 +0900

    0.6.9

--- wayfarer-map-extension.user.ts
+++ wayfarer-map-extension.user.ts
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         wayfarer-map-extension
 // @namespace    http://tampermonkey.net/
-// @version      0.6.8
+// @version      0.6.9
 // @description  A user script that extends the official Niantic Wayfarer map.
 // @author       Wiinuk
 // @match        https://wayfarer.nianticlabs.com/new/mapview
 // @match        https://wayfarer.nianticlabs.com/new/mapview?*
-// @grant        GM.xmlhttpRequest
+// @grant        GM.xmlHttpRequest
 // @connect      *
 // ==/UserScript==
 
