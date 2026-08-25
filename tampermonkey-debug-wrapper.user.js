c432b92c784b3d9d2b34fe244c8342262b2e7d3c
wiinuk <wii.nukegara@gmail.com>
2026-08-26 06:07:47 +0900

    update url

--- tampermonkey-debug-wrapper.user.js
+++ tampermonkey-debug-wrapper.user.js
@@ -3,7 +3,8 @@
 // @description  A debug wrapper for referencing local scripts
 // @version      1.0
 // @namespace    http://tampermonkey.net/
-// @match        https://wayfarer.nianticlabs.com/new/mapview
+// @match        https://wayfarer.scopely.com/new/mapview
+// @match        https://wayfarer.scopely.com/new/mapview?*
 // @require      file:////wsl.localhost/<path to your local repository>/wayfarer-map-extension.user.js
 // @grant        none
 // ==/UserScript==
--- wayfarer-map-extension.user.ts
+++ wayfarer-map-extension.user.ts
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         wayfarer-map-extension
 // @namespace    http://tampermonkey.net/
-// @version      0.6.10
+// @version      0.7.1
 // @description  A user script that extends the official Niantic Wayfarer map.
 // @author       Wiinuk
-// @match        https://wayfarer.nianticlabs.com/new/mapview
-// @match        https://wayfarer.nianticlabs.com/new/mapview?*
+// @match        https://wayfarer.scopely.com/new/mapview
+// @match        https://wayfarer.scopely.com/new/mapview?*
 // @grant        GM.xmlHttpRequest
 // @connect      *
 // ==/UserScript==
